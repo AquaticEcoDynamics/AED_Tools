1b7b23d561382eae4ed0df43accdeaf7905d724b
Casper Boon <casper.boon@uwa.edu.au>
2016-03-07 14:04:44 +0800

    updated for latest FABM

--- fetch_sources.sh
+++ fetch_sources.sh
@@ -100,13 +100,7 @@ fi
 
 if [ "$GETFABM" = "true" ] ; then
   if [ ! -d fabm-git ] ; then
-    if [ `which wget > /dev/null` ] ; then
-      curl -O http://aed.see.uwa.edu.au/research/models/Extras/fabm-git-20141205.tar.gz
-    else
-      wget http://aed.see.uwa.edu.au/research/models/Extras/fabm-git-20141205.tar.gz
-    fi
-    tar xzf fabm-git-20141205.tar.gz
-    mv fabm-git-20141205 fabm-git
+    git clone git://git.code.sf.net/p/fabm/code fabm-git
   fi
 fi
 
