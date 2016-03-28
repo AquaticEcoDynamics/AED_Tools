e1c123a59ad573ae1eea38c2deea33c8f295c041
Casper <casper.boon@uwa.edu.au>
2016-03-28 17:24:06 +0800

    dev

--- admin/status_check.sh
+++ admin/status_check.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-echo '*** checking status for .'
+echo '*** checking status for . from' `cat .git/config | grep -w url`
 git status
 
 CWD=`pwd`
 for src in libplot libutil libaed2 GLM ; do
-   echo '*** checking status for' $src
+   echo '*** checking status for' $src ' from ' `cat $src/.git/config | grep -w url`
    if [ -d $src ] ; then
      cd $src
      git status
