6f76fad73af52079e2ce4b5965a8166cc0fbe22f
Casper Boon <casper.boon@uwa.edu.au>
2016-04-05 10:52:32 +0800

    status check update

--- admin/status_check.sh
+++ admin/status_check.sh
@@ -10,7 +10,7 @@ check_remote () {
   BRANCH=`git branch | cut -f2 -d\ `
 # echo "on branch $BRANCH"
   LOCAL=`git show-ref -s refs/remotes/origin/$BRANCH`
-  REMOTE=`git ls-remote --heads 2> /dev/null | grep $BRANCH | cut -f1`
+  REMOTE=`git ls-remote --heads $REPO 2> /dev/null | grep $BRANCH | cut -f1`
 # echo local SHA1 is $LOCAL remote SHA1 is $REMOTE
   if [ "$LOCAL" != "$REMOTE" ] ; then
     echo "an update is available"
@@ -22,16 +22,20 @@ check_remote () {
 
 echo '*** checking status for . from' `cat .git/config | grep -w url`
 git status
-if [ $verbose ] ; then check_remote ; fi
+REPO=`cat .git/config | grep -w url | cut -f2 -d=`
+if [ $verbose ] ; then check_remote $REPO; fi
 
 for src in libplot libutil libaed2 GLM libfvaed2 ; do
   if [ -d $src ] ; then
+    REPO=`cat $src/.git/config | grep -w url | cut -f2 -d=`
     echo "===================================================="
-    echo '*** checking status for ['$src'] from ' `cat $src/.git/config | grep -w url`
+    echo "*** checking status for [$src] from $REPO"
     cd $src
     git status
-    if [ $verbose ] ; then check_remote ; fi
+    if [ $verbose ] ; then check_remote $REPO; fi
     cd $CWD
+  else
+    echo "no directory for $src"
   fi
 done
 
