d5d1c9e445f18906385368cff6c66225a0e32715
Casper Boon <casper.boon@uwa.edu.au>
2017-06-20 16:09:11 +0800

    improved fetch script

--- fetch_sources.sh
+++ fetch_sources.sh
@@ -1,16 +1,24 @@
 #!/bin/sh
+#-------------------------------------------------------------------------------
+#  Script to fetch sources for the tools and utilities
+#-------------------------------------------------------------------------------
 
 rep_list=""
+upd_list=""
+
 if [ $# = 0 ] ; then
-  # The default case is to fetch only libaed2
-  GETAED2="true"
+  # The default case is to just update
+  GETAED2="false"
   GETPLOT="false"
   GETUTIL="false"
   GETFABM="false"
   GET_GLM="false"
   GETPLUS="false"
   GETFVAED="false"
+  upd_list="libaed2 libplot libutil GLM libfvaed2 libaed2-plus"
 fi
+
+#-------------------------------------------------------------------------------
 
 while [ $# -gt 0 ] ; do
   #echo $# : $1
@@ -70,26 +78,7 @@ if [ "$GETPLUS" = "true" ]  ; then rep_list="$rep_list libaed2-plus" ; fi
 if [ "$GETPLOT" = "true" ]  ; then rep_list="$rep_list libplot" ; fi
 if [ "$GETUTIL" = "true" ]  ; then rep_list="$rep_list libutil" ; fi
 
-#echo list = $rep_list
-
-
-if [ "$GITHOST" = "" -a "$rep_list" != "" ] ; then
-  REPOS=`grep -w url .git/config | cut -d\  -f3`
-  NWORDS=`echo $REPOS | cut -d: -f2 | sed 's:/: :g' | wc -w`
-
-  if [ "`echo $REPOS | grep '@'`" != "" ] ; then
-    if [ $NWORDS = 1 ] ; then #
-      GITHOST=`echo $REPOS | cut -d: -f1`:
-    else
-      NWORDS=`expr $NWORDS - 1`
-      GITHOST=`echo $REPOS |  cut -d\/ -f-$NWORDS`/
-    fi
-  else
-    NWORDS=`expr $NWORDS + 1`
-    GITHOST=`echo $REPOS |  cut -d\/ -f-$NWORDS`/
-  fi
-fi
-#echo GITHOST is $GITHOST
+#-------------------------------------------------------------------------------
 
 fetch_it () {
   src=$1
@@ -109,20 +98,78 @@ fetch_it () {
   fi
 }
 
+#-------------------------------------------------------------------------------
 
-if [ "$rep_list" != "" ] ; then
+if [ "$upd_list" != "" ] ; then
   echo "updating AED_Tools from " `grep -w url .git/config`
   git pull
+
+  count=0
+  for src in $upd_list ; do
+    if [ -d $src ] ; then
+      count=$((count+1))
+      echo "updating $src from " `grep -w url $src/.git/config`
+
+      cd $src
+      git pull # origin master
+      cd ..
+    fi
+  done
+elif [ "$rep_list" != "" ] ; then
+  # echo list = $rep_list
+
+  if [ "$GITHOST" = "" ] ; then
+    REPOS=`grep -w url .git/config | cut -d\  -f3`
+    NWORDS=`echo $REPOS | cut -d: -f2 | sed 's:/: :g' | wc -w`
+
+    if [ "`echo $REPOS | grep '@'`" != "" ] ; then
+      if [ $NWORDS = 1 ] ; then #
+        GITHOST=`echo $REPOS | cut -d: -f1`:
+      else
+        NWORDS=`expr $NWORDS - 1`
+        GITHOST=`echo $REPOS |  cut -d\/ -f-$NWORDS`/
+      fi
+    else
+      NWORDS=`expr $NWORDS + 1`
+      GITHOST=`echo $REPOS |  cut -d\/ -f-$NWORDS`/
+    fi
+  fi
+  # echo GITHOST is $GITHOST
 
   for src in $rep_list ; do
     fetch_it $src
   done
 fi
 
+#-------------------------------------------------------------------------------
+
 if [ "$GETFABM" = "true" ] ; then
   if [ ! -d fabm-git ] ; then
+    echo "===================================================="
+    echo "fetching fabm from git://git.code.sf.net/p/fabm/code"
     git clone git://git.code.sf.net/p/fabm/code fabm-git
   fi
+fi
+
+#-------------------------------------------------------------------------------
+if [ $count = 0 ] ; then
+  echo "There do not seem to be any repositories requested or present"
+  echo "Usage : "
+  echo "  fetch_sources.sh [-g <githost>] <repo>"
+  echo
+  echo "where <repo> can be one or more of :"
+  echo "  glm     : get glm [and it's dependancies]"
+  echo "  libaed2 : fetch the libaed2 sources"
+  echo "  libplot : fetch the libplot sources"
+  echo "  libutil : fetch the libutil sources"
+  echo "  plus    : fetch the libaed2-plus sources (private repository)"
+  echo "  fvaed2  : fetch the libfvaed2 sources"
+  echo "  fabm    : fetch the fabm sources (possible dependancy for glm)"
+  echo
+  echo "  all     : fetch them all"
+  echo
+  echo "  -g|--githost <githost> : allows you to specify a different githost"
+  echo "          The default is https://github.com/AquaticEcoDynamics/"
 fi
 
 exit 0
